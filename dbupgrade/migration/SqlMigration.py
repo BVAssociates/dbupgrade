8d392cffeb95a9811c176b60d3f8dd2c64f2b88d
Vincent Bauchart <bauchart@bvassociates.fr>
2014-12-04 22:53:53 +0100

    implement BaseMigration with minimal actions with migration

--- dbupgrade/migration/SqlMigration.py
+++ dbupgrade/migration/SqlMigration.py
@@ -1,7 +1,67 @@
+from dbupgrade.common import Migration
+
 __author__ = 'vincent'
 
 
-class SqlMigration(object):
+class BaseMigration(object):
+    def initialize(self):
+        """
+        This method setup
+        :return: string
+        """
+        return ''
+
+    def is_initialized(self):
+        """
+
+        :return: string
+        """
+        return ''
+
+    def run_migration(self, migration):
+        """
+        This is the main method
+        Run all the migrations steps
+        :param migration: Migration
+        :return: str
+        """
+        assert isinstance(migration, Migration)
+
+        result_string = ''
+
+        result_string += self.begin()
+
+        for step_version in migration:
+            result_string += self.single_migration(migration[step_version])
+
+        result_string += self.end()
+
+        return result_string
+
+    def single_migration(self, text):
+        """
+        Run a single migration step
+        :param text: str
+        :return:
+        """
+        return text + "\n"
+
+    def begin(self):
+        """
+        This method is executed before running any migration step
+        :return:
+        """
+        return ''
+
+    def end(self):
+        """
+        This method is executed after all migration steps has been executed
+        :return:
+        """
+        return ''
+
+
+class SqlMigration(BaseMigration):
     """
 
     """
@@ -9,31 +69,31 @@ class SqlMigration(object):
     def __init__(self):
         pass
 
-    def migration_join(self, migration_list):
-        """
-        return a string with the whole migration
-        :param migration_list:
-        :return string:
-        """
+    def initialize(self):
+        initialization_sql = ('CREATE TABLE public.dbupgrade_history (\n'
+                              '    schema  VARCHAR(90) NOT NULL,\n'
+                              '    version VARCHAR(90) NOT NULL,\n'
+                              '    timestamp DATE NOT NULL DEFAULT CURRENT_DATE\n'
+                              ');\n'
+        )
+        return initialization_sql
 
-        result_string = ''
+    def is_initialized(self):
+        check_init = "SELECT schema,version,timestamp FROM public.dbupgrade_history LIMIT 1;\n"
+        return check_init
 
-        result_string += self.migration_begin()
-
-        for migration in migration_list:
-            result_string += self.migration_string(migration)
-            result_string += self.set_version()
-
-        result_string += self.migration_end()
-
-    def migration_begin(self):
+    def begin(self):
         return 'BEGIN;' + "\n"
 
-    def migration_string(self, migration):
-        return migration + "\n"
+    def single_migration(self, text):
+        return text + "\n" + self.set_version() + "\n"
 
-    def migration_end(self):
+    def end(self):
         return 'COMMIT;' + "\n"
 
-    def set_version(self):
-        return ''
+    def set_version(self, version):
+        return 'INSERT INTO public.dbupgrade_history SET (schema,version) VALUES (\'%s\',\'%s\');' % ('tata', 'titi')
+
+
+class MigrationNotInitialized(Exception):
+    pass
---
+++ test/test_migration.py
@@ -0,0 +1,59 @@
+from dbupgrade.common import StepVersion
+from dbupgrade.migration.SqlMigration import SqlMigration, BaseMigration
+from dbupgrade.repository.file_repository import FileRepository
+
+__author__ = 'Vincent'
+
+import unittest
+
+
+class BaseMigrationCase(unittest.TestCase):
+    def setUp(self):
+        self.simple_migration = BaseMigration()
+
+    def test_run_migration_upgrade(self):
+        sample_repo = FileRepository('repository', 'app2')
+        sample_migration = sample_repo.get_migration(version_from=StepVersion('4.0.1'),
+                                                     version_to=StepVersion('4.10.0'))
+
+        result = self.simple_migration.run_migration(sample_migration)
+        self.assertEqual(
+            result,
+            ("CREATE TABLE test_first (INTEGER a,VARCHAR b);\n"
+             "CREATE TABLE test_second (INTEGER a,VARCHAR b);\n"
+             "ALTER TABLE testfirst ADD COLUMN INTEGER C;\n"
+            )
+        )
+
+    def test_run_migration_downgrade(self):
+        repo = FileRepository('repository', 'app2')
+        sample_migration = repo.get_migration(version_from=StepVersion('4.10.0'), version_to=StepVersion('4.0.1'))
+
+        result = self.simple_migration.run_migration(sample_migration)
+        self.assertEqual(
+            result,
+            ("ALTER TABLE testfirst DROP COLUMN C;\n"
+             "DROP TABLE test_second;\n"
+             "DROP TABLE test_first;\n"
+            )
+        )
+
+
+class SqlMigrationCase(unittest.TestCase):
+    def setUp(self):
+        self.sqlmigration = SqlMigration()
+
+    def test_initialize(self):
+        self.assertEqual(
+            self.sqlmigration.initialize(),
+            '''CREATE TABLE public.dbupgrade_history (
+    schema  VARCHAR(90) NOT NULL,
+    version VARCHAR(90) NOT NULL,
+    timestamp DATE NOT NULL DEFAULT CURRENT_DATE
+);
+'''
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
