6cbc9b5b288bba41e9cf4a4ff72d4e4b9036f8a1
Vincent Bauchart <bauchart@bvassociates.fr>
2014-12-07 18:53:28 +0100

    setup.py skeleton

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dbupgrade',
+    version='0.1',
+    packages=['dbupgrade', 'dbupgrade.migration', 'dbupgrade.repository'],
+    url='',
+    license='',
+    author='vincent',
+    author_email='',
+    description=''
+)
